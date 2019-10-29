fbce96d3e706fa3cdbc4a2d21c289efa40380463
Yaron Weiss <yaron.weiss@amdocs.com>
2019-10-28 20:26:50 -0500

    Adding program and making it plugin

---
+++ GingerOracleDB/Program.cs
@@ -0,0 +1,29 @@
+﻿using Amdocs.Ginger.CoreNET.Drivers.CommunicationProtocol;
+using Amdocs.Ginger.Plugin.Core;
+using System;
+
+namespace Oracle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Oracle Database plugin";
+            Console.WriteLine("Starting Oracle Database Plugin");
+
+            using (GingerNodeStarter gingerNodeStarter = new GingerNodeStarter())
+            {
+                if (args.Length > 0)
+                {
+                    gingerNodeStarter.StartFromConfigFile(args[0]);  // file name 
+                }
+                else
+                {                    
+                    gingerNodeStarter.StartNode("Oracle Service 1", new GingerOracleConnection(), SocketHelper.GetLocalHostIP(), 15001);                    
+                }
+                gingerNodeStarter.Listen();
+            }
+
+        }
+    }
+}
